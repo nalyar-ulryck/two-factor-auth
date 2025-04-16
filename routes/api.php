e02c49d33139ec06e35409bbb36261f2691664d5
nalyar <raylanzeeroo@outlook.com>
2025-04-16 13:31:57 -0300

    fix: fix: add auth:sanctum in the API route.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use NalyarUlryck\TwoFactorAuth\Http\Controllers\TwoFactorController;
 use PHPUnit\Framework\Attributes\Group;
 
-Route::middleware(['api','auth'])->prefix('api')->group(function () {
+Route::middleware(['api','auth:sanctum'])->prefix('api')->group(function () {
     Route::prefix('twofactor')->group(function () {
 
         Route::get('/enable-2fa', [TwoFactorController::class, 'enable2fa'])->name('enable2fa');
